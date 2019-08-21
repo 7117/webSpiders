ba41bdbdc7cffcb94d240c3473332d1aa5666404
7117 <qinglangsalaheiyou@163.com>
2019-08-21 21:53:57 +0800

    beego路由配置

--- src/web/controllers/default.go
+++ src/web/controllers/default.go
@@ -8,8 +8,12 @@ type MainController struct {
 	beego.Controller
 }
 
-func (c *MainController) Get() {
+func (c *MainController) Test() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+}
+
+func (c *MainController) Post() {
+	c.Ctx.WriteString("hello");
 }
--- src/web/routers/router.go
+++ src/web/routers/router.go
@@ -6,5 +6,7 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{},"get:Get")
+	beego.Router("/", &controllers.MainController{},"post:Post")
+
 }
