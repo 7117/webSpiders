f10a730ff332fd7211a5e0c765cbdf28172cf105
7117 <qinglangsalaheiyou@163.com>
2019-08-21 23:02:22 +0800

    beego:会话机制

---
+++ src/web/controllers/testlogin.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type TestLoginController struct {
+	beego.Controller
+}
+
+type User2 struct{
+	Username string
+	Password string
+}
+
+func (c *TestLoginController) Login() {
+	name:=c.Ctx.GetCookie("name");
+	password:=c.Ctx.GetCookie("password");
+
+	if(name!=""){
+		c.Ctx.WriteString("Username:" + name + " Password:" + password)
+	}else{
+		c.Ctx.WriteString(`<html><form action="http://127.0.0.1:8080/testlogin" method="post"> 
+		<input type="text" name="Username"/>
+		<input type="password" name="Password"/>
+		<input type="submit" value="submit"/>
+		</form></html>`)
+	}
+}
+
+func (c *TestLoginController) Post(){
+	u := User2{}
+	if err:=c.ParseForm(&u) ; err != nil{
+
+	}
+
+	c.Ctx.SetCookie("name", u.Username, 100, "/")
+	c.Ctx.SetCookie("password", u.Password, 100, "/")
+	c.Ctx.WriteString("Username:" + u.Username + " Password:" + u.Password)
+}
--- src/web/routers/router.go
+++ src/web/routers/router.go
@@ -10,5 +10,5 @@ func init() {
 	beego.Router("/", &controllers.MainController{},"post:Post")
 	beego.Router("/test", &controllers.TestController{}, "get:Get;post:Post")
 	beego.Router("/testinput", &controllers.TestInputController{}, "get:Get;post:Post")
-	beego.Router("/testlogin", &controllers.TestLoginController{}, "*:Login")
+	beego.Router("/testlogin", &controllers.TestLoginController{}, "get:Login;post:Post")
 }
